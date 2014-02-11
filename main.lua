8d8a1afb9a270aa4231667590dc6ec374ec51578
finnbar <finnjkeating@aol.com>
2014-02-11 19:54:31 +0000

    Added a background, win/lose messages and lots of placeholder graphics

--- main.lua
+++ main.lua
@@ -2,13 +2,19 @@ block = love.graphics.newImage("img/block.png")
 obstacle = love.graphics.newImage("img/obstacle.png")
 stairs = love.graphics.newImage("img/stairsdown.png")
 stairsup = love.graphics.newImage("img/stairsup.png")
+bg = love.graphics.newImage("img/bg1.png")
+winImg,loseImg = love.graphics.newImage("img/win.png"),love.graphics.newImage("img/lose.png")
+bgData = bg:getData()
+choppyPixels = {}
 sp = {160,20}
-players = {{floor=1,x=1,y=1,image=love.graphics.newImage("img/playera.png"),fall=0,velocity=5,rot=0,falling=false,damaging=false,health=100},{floor=1,x=5,y=5,image=love.graphics.newImage("img/playerb.png"),fall=0,velocity=5,rot=0,falling=false,damaging=false,health=100}}
+players = {{floor=1,x=1,y=1,image=love.graphics.newImage("img/playera.png"),fall=0,velocity=5,rot=0,falling=false,damaging=false,health=100,losingHealth=0},{floor=1,x=5,y=5,image=love.graphics.newImage("img/playerb.png"),fall=0,velocity=5,rot=0,falling=false,damaging=false,health=100,losingHealth=0}}
 map = {}
 mapSize = 80
 prev = {1,1}
 started = false
 cols = {{255,0,0},{0,255,0}}
+death = 0
+deathX,deathY = 1,1 --death animation
 
 --[[
 Notes so far:
@@ -59,6 +65,27 @@ a+1==y pos
 ]]
 
 function love.draw()
+	local avg,favg,n = 0,0,0
+	for i in pairs(players) do
+		n=n+1
+		avg=avg+players[i].floor
+		favg=favg+players[i].fall
+	end
+	avg = avg/n
+	favg = favg/n
+	love.graphics.setColor(255,255,255,230)
+	love.graphics.draw(bg,0,((avg/mapSize)+((favg/280)/mapSize))*-200)
+	if death~=0 then
+		for i=1,400,1 do
+			table.insert(choppyPixels,{love.math.random(0,49),love.math.random(0,99)})
+		end
+		for p in pairs(choppyPixels) do
+			if death==2 and choppyPixels[p][1]<50 then choppyPixels[p][1]=choppyPixels[p][1]+50 end
+			local r,g,b = bgData:getPixel((choppyPixels[p][1]*10),(choppyPixels[p][2]*10))
+			love.graphics.setColor(255-r,255-g,255-b,230)
+			love.graphics.rectangle("fill", choppyPixels[p][1]*10, choppyPixels[p][2]*10,10,10)
+		end
+	end
 	for i=0,#players-1 do
 		for z=#map,1,-1 do
 			if z==players[i+1].floor then love.graphics.setColor(255,255,255,255) else
@@ -76,12 +103,14 @@ function love.draw()
 							elseif map[z][x+a][a+1]==6 then
 								love.graphics.draw(stairs,sp[1]+(x*40.5)+(i*500),sp[2]+(x*24)+(a*48)+((z+3)*130)-(players[i+1].floor*130)-escalateAbove(i+1,z)+players[i+1].fall-44,0,0.45,0.45)
 							elseif map[z][x+a][a+1]==7 then
-							  love.graphics.draw(stairsup,sp[1]+(x*40.5)+(i*500),sp[2]+(x*24)+(a*48)+((z+3)*130)-(players[i+1].floor*130)-escalateAbove(i+1,z)+players[i+1].fall-44,0,0.45,0.45)
+								love.graphics.draw(stairsup,sp[1]+(x*40.5)+(i*500),sp[2]+(x*24)+(a*48)+((z+3)*130)-(players[i+1].floor*130)-escalateAbove(i+1,z)+players[i+1].fall-44,0,0.45,0.45)
 							end
 						end
 						for k in pairs(players) do
-							love.graphics.circle("fill", 460+(k*20),((players[k].floor/mapSize)*750)-((players[k].fall/280)*(1/mapSize))*800-((mapSize^2)/200)+(mapSize/4)+20,5,4)
 							local col1,col2,col3,col4 = love.graphics.getColor()
+							love.graphics.setColor(0,0,0)
+							love.graphics.circle("fill", 460+(k*20),(((players[k].floor/mapSize)*750)-((players[k].fall/280)*(1/mapSize))*750)+20,5,4)
+							love.graphics.setColor(255,255,255)
 							local fal = 0
 							if k~=i+1 then fal=players[i+1].fall love.graphics.setColor(255,255,255,200) end
 							if players[k].x==x+a and players[k].y==a+1 and players[k].floor==z then
@@ -98,10 +127,19 @@ function love.draw()
 			end
 		end
 	love.graphics.setColor(cols[i+1][1],cols[i+1][2],cols[i+1][3],255)
-	love.graphics.rectangle("fill",500*i, 0, (players[i+1].health/100)*500, 10)
+	if players[i+1].health>=0 then love.graphics.rectangle("fill",500*i, 0, (players[i+1].health/100)*500, 10) end
 	end
 	love.graphics.setColor(255,255,255,255)
-	love.graphics.print(love.timer.getAverageDelta(),900,12)
+	love.graphics.print(love.timer.getDelta(),900,12)
+	if death>0 then
+		if death%2==0 then --p2 lose
+			love.graphics.draw(loseImg,600,100)
+			love.graphics.draw(winImg,50,100)
+		else
+			love.graphics.draw(winImg,600,100)
+			love.graphics.draw(loseImg,50,100)
+		end
+	end
 end
 
 function love.update(dt)
@@ -113,13 +151,22 @@ function love.update(dt)
 	else
 		for i in pairs(players) do
 			players[i].health=players[i].health+dt
+			if players[i].losingHealth>0 then
+				if players[i].losingHealth>10 then --buggy
+					players[i].losingHealth=players[i].losingHealth-10
+					players[i].health=players[i].health-10
+				else
+					players[i].health=players[i].health-players[i].losingHealth
+					players[i].losingHealth=0
+				end
+			end
 			if players[i].health>100 then players[i].health=100 end
 			if players[i].health<=0 then
 				if i==1 then win(2) else win(1) end
 			end
 			players[i].falling=false
 			if players[i].fall~=0 then players[i].falling=true end
-			if players[i].fall~=0 then players[i].rot=players[i].rot+200*dt else players[i].rot=0 end
+			if players[i].fall~=0 then players[i].rot=players[i].rot+players[i].velocity*dt*2 else players[i].rot=0 end
 			if map[players[i].floor][players[i].x][players[i].y]==0 then
 				if players[i].floor<mapSize then
 					players[i].falling=true
@@ -215,9 +262,10 @@ end
 
 function damage(player,damage)
 	print("player "..player.." took "..damage.." damage")
-	players[player].health=players[player].health-damage
+	players[player].losingHealth=damage
 end
 
 function win(player)
 	print("player "..player.." wins! (this bit needs to be coded)")
+	if player==1 then death=2 else death=1 end
 end
